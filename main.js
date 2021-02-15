0457509cb0e64307d4a220ee43e94e8e83a0ee6d
Jeptzy <77440578+Jeptzy@users.noreply.github.com>
2021-02-15 23:00:20 +0800

    hey

--- main.js
+++ main.js
@@ -27,5 +27,42 @@ function closenav(){
     }) 
 }
 
-   
+//scroll reveal
+// ScrollReveal().reveal('.headline');
+// ScrollReveal().reveal('.home_text', { delay: 500 });
+// ScrollReveal().reveal('.home_svg', { delay: 2000 })
+
+// ScrollReveal({ distance: '100px' });
+// ScrollReveal().reveal('.home_text', { origin: 'left' , delay: 500});
+// ScrollReveal().reveal('.home_svg', { origin: 'right',delay: 500});
+
+// ScrollReveal().reveal('.func_text', { origin: 'right' , delay: 700});
+// ScrollReveal().reveal('.collab_text', { origin: 'right',delay: 900});
+
+// ScrollReveal().reveal('.col1', { origin: 'bottom' , delay: 700});
+// ScrollReveal().reveal('.col2', { origin: 'bottom',delay: 900});
+// ScrollReveal().reveal('.col3', { origin: 'bottom',delay: 1000});
+
+const sr = ScrollReveal({
+    
+    distance: '80px',
+    duration: 2000,
+    reset: false
+})
+
+sr.reveal('.home_text', { origin: 'left' , delay: 500});
+sr.reveal('.home_svg', { origin: 'right',delay: 500});
+
+sr.reveal('.about_svg', { origin: 'left' , delay: 700});
+sr.reveal('.func_text', { origin: 'right' , delay: 700});
+sr.reveal('.collab_text', { origin: 'right',delay: 900});
+
+sr.reveal('.col1', { origin: 'bottom' , delay: 500});
+sr.reveal('.col2', { origin: 'bottom',delay: 700});
+sr.reveal('.col3', { origin: 'bottom',delay: 1000});
+
+sr.reveal('.contact_title', {delay: 1000});
+
+
+
 
